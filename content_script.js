1d6112cbab556993ddb3c18d7e86cf03ec3545d7
oyama <52624121+yoyama92@users.noreply.github.com>
2024-01-26 14:13:35 +0900

    高さが変わってしまったので戻す

--- content_script.js
+++ content_script.js
@@ -1,3 +1,3 @@
 Array.from(document.querySelector("ul.gb_actors_list")?.querySelectorAll("li") ?? []).forEach(n => {
-  n.querySelector("a").innerHTML += `<span style="font-size: 16px"> ${n.classList[2].substring(1)} </span>`;
+  n.querySelector("a").textContent += ` ${n.classList[2].substring(1)}`;
 });
